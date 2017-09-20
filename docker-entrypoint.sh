209cb3e30f55b0fc458b3afe366a00295a25b170
Roland Huß <roland@ro14nd.de>
2017-09-20 15:46:22 +0200

    Initial commit, from https://github.com/jimmidyson/jboss-dockerfiles-keycloak/tree/master/server
    branch: openshift-compatible

---
+++ docker-entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ $KEYCLOAK_USER ] && [ $KEYCLOAK_PASSWORD ]; then
+    keycloak/bin/add-user-keycloak.sh --user $KEYCLOAK_USER --password $KEYCLOAK_PASSWORD
+fi
+
+exec /opt/jboss/keycloak/bin/standalone.sh $@
